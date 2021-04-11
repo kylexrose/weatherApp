74360e304518f70e7e2ba97a266dd2bd4819f20a
kylexrose@gmail.com <kylexrose@gmail.com>
2021-04-10 21:13:57 -0400

    html and css complete for base functionality

---
+++ colorScheme.js
@@ -0,0 +1,9 @@
+const colors = {
+    blue: {
+        "#navbar" : "#066f94",
+        "#searchBar" : "#488baa",
+        "body" : ["#69a9bf", "#f7fbfc"],
+        "#historyBar" : "#488baa",
+        "#forcastBar" : "#0b4259"
+    }
+}
