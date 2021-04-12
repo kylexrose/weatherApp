17ecacea6318b981b0c79d5450c7b085b2e7f2e2
kylexrose@gmail.com <kylexrose@gmail.com>
2021-04-12 02:44:53 -0400

    added menuRight file

---
+++ menuRight.js
@@ -0,0 +1,12 @@
+const menuRight = {
+    "WEATHER":  ["Today's Forecast", "Hourly Forecast", "10 Day Forecast", "Weekend Forecast",
+                "Monthly Forecast", "National Forecast", "National News", "Almanac"],
+    "RADAR":    ["Weather in Motion", "Radar Maps", "Classic Weather Maps", "Regional Satellite"],
+    "SEVERE":   ["Severe Alerts", "Safety & Preparedness", "Hurricane Central"],
+    "ACCOUNT": ["Subscribe", "Have a Premium account?", "Log in"],
+    "VIDEO & PHOTOS": ["Top Stories", "Video", "Photos", "Climate News", "Award-Winning Investigations"],
+    "HEALTH & ACTIVITIES": ["Allergy Tracker", "Cold & Flu", "Water Scarcity", "Air Quality Forecast"],
+    "TV & WEATHER": ["Download Apps", "Alexa Skill", "Watch Live", "Personalities"],
+    "WEATHER PRODUCTS": ["Alexa Skill"],
+    "PRIVACY": ["Privacy Settings", "DataRights", "Privacy Policy"]
+}
